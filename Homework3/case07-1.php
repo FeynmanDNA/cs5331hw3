297a3007ca725a2c66fabb82c6ebee5ae527e27f
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2020-03-30 17:30:21 +0800

    eploit07 redirect

--- Homework3/case07-1.php
+++
@@ -1,7 +0,0 @@
-<h1>Case 07</h1>
-<h2></h2>
-
-<div id="demo">
-	This page is the new site. Old site is found over 
-	<a href="case07.php">here</a>
-</div>
--- Homework3/case07.php
+++
@@ -1,9 +0,0 @@
-<h1>Case 07</h1>
-<h2></h2>
-
-<?php 
-		$redirect_url = 'case07-1.php';
-		header("Location: " . $redirect_url); 
-?>
-
-<div id="demo">The flag is v6xAT3M7Ab67RDy</div>
